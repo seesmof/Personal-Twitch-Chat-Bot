4890dd9ad3fcdff39a6fcb2799075f4776857e65
Oleh Onyshchenko <seesmwork@gmail.com>
2023-03-07 17:15:25 +0200

    Revert "feat: :sparkles: add function that greets each user, who sent their first message in chat"
    This reverts commit 36b0f7cf3edde32f3f5d8e8eb7b9772f081f691f.

--- bot_dev.py
+++ bot_dev.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 from os.path import join, dirname
 from twitchio.ext import commands
 from playsound import playsound
-from datetime import datetime
 
 import openai
 import random
+import datetime
 import os
 
 TMI_TOKEN = "oauth:0purffc2ao53hdg254j26okkj1fh76"
@@ -25,16 +25,21 @@ bot = commands.Bot(
     initial_channels=[CHANNEL]
 )
 
-# for handling logging messages in an appropriate folder
-log_dir = "D:\GitHub\python-twitchio-chat-bot\logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-# for handling sound file location
-sound_path = "D:\GitHub\python-twitchio-chat-bot\sound.mp3"
-# for handling OpenAI API key
-openai.api_key = "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"
-# for handling OpenAI model name
-model_engine = "gpt-3.5-turbo"
+
+@bot.event
+async def event_stream_online(self, stream):
+    channel = await self.get_channel(CHANNEL)
+    bot.run()
+    await channel.send("Всім привіт!")
+
+
+@bot.event
+async def event_ready():
+    # print bot and channel name when it activates
+    print(f"{BOT_NICK} is online at {CHANNEL}!")
+    # log the start message
+    write_to_log(f"is online at {CHANNEL}", "    BOT")
+
 # create a list of greetings
 greetings_ua = ["Здоров!", "Привіт!", "Вітаю!",
                 "Вітання!", "Як ся маєш?", "Слава Україні!", "Як воно?", "Бажаю здоров'я!", "Радий вітати!", "Радий бачити!", "Як справи?", "Як здоров'я?"]
@@ -45,33 +50,12 @@ last_message_time = {}
 
 
 @bot.event
-async def event_ready():
-    # print bot and channel name when it activates
-    print(f"{BOT_NICK} is online at {CHANNEL}!")
-
-
-@bot.event
 async def event_message(ctx):
     # the bot should not react to itself
     if ctx.author.name.lower() == BOT_NICK.lower():
         # print out bot's message to log
         print(f"\nBOT: {ctx.content}")
         return
-
-    global last_message_time
-    user = ctx.author.name
-    if user not in last_message_time:
-        # user is sending the first message of the day
-        last_message_time[user] = datetime.now()
-        await ctx.channel.send(f"@{user}, {random.choice(greetings_ua)} Ласкаво просимо KonCha")
-    else:
-        # user has sent a message before
-        last_time = last_message_time[user]
-        today = datetime.now().date()
-        if last_time.date() < today:
-            # user is sending the first message of the day
-            last_message_time[user] = datetime.now()
-            await ctx.channel.send(f"@{user}, {random.choice(greetings_ua)} Ласкаво просимо KonCha")
 
     # for handling ChatGPT requests from chat
     if ctx.content.startswith("@wuyodo"):
@@ -125,13 +109,20 @@ async def generate_response(input_text):
     return output_text
 
 
-def check_for_letters(text, letters):
-    # for each letter in letters list
-    for letter in letters:
-        # check if letter is in the list
-        if letter in text:
-            return True
-    return False
+def write_to_log(message, author):
+    # for handling current time
+    now = datetime.now()
+    # for handling the file name
+    file_name = "log_" + now.strftime("%d-%m-%Y") + ".txt"
+    # for handling the file path
+    file_path = os.path.join(log_dir, file_name)
+    # open file with appropriate decoding
+    with open(file_path, "a", encoding="utf-8") as log_file:
+        # declare and output timestamp before message
+        timestamp = datetime.now().strftime('%H:%M:%S')
+        log_file.write(timestamp)
+        # output message with author name to log
+        log_file.write(f"\n{author}: {message}\n\n")
 
 
 @bot.command(name='інфа')
@@ -184,8 +175,11 @@ async def say_hi_Ukrainian(ctx):
     if "@" not in username:
         # then set username to user who sent the message
         username = '@' + ctx.author.name
+    # create a list of greetings
+    greetings = ["Здоров!", "Привіт!", "Вітаю!",
+                 "Вітання!", "Як ся маєш?", "Слава Україні!", "Як воно?", "Бажаю здоров'я!", "Радий вітати!", "Радий бачити!", "Як справи?", "Як здоров'я?"]
     # output the greeting message and tag the user
-    await ctx.send(f"{username}, {random.choice(greetings_ua)}")
+    await ctx.send(f"{username}, {random.choice(greetings)}")
 
 
 @bot.command(name='hi')
@@ -196,8 +190,11 @@ async def say_hi_English(ctx):
     if "@" not in username:
         # if not set username to the user who sent the message
         username = '@' + ctx.author.name
+    # create a list of greetings
+    greetings = ["Hey!", "What's up?", "Yo!", "Greetings!", "Hi there!", "Howdy!", "How's it going?", "What's new?",
+                 "Good day!", "What's happening?", "Sup?", "How's everything?", "What's up, buddy?", "Good to see you!"]
     # output the greeting message and tag the user
-    await ctx.send(f"{username}, {random.choice(greetings_en)}")
+    await ctx.send(f"{username}, {random.choice(greetings)}")
 
 
 @bot.command(name='гам')
