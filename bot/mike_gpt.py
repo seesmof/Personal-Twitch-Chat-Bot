424fedd5a1eafd3d4e1d2fac2733b10f53910eee
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-18 09:58:22 +0000

    Tuesday, April 18, 2023, 12:58:22 PM Eastern European Summer Time

--- bot/mike_gpt.py
+++ bot/mike_gpt.py
@@ -1,3 +1,5 @@
+from vars import *
+import mfs
 from twitchio.ext import commands
 from datetime import datetime
 import openai
@@ -6,8 +8,9 @@ import asyncio
 import time
 import requests
 import re
-import mfs
-from vars import *
+import sys
+parent_dir = os.path.abspath("..")
+sys.path.append(parent_dir)
 
 BOT_PREFIX = "!"
 CHANNEL = "mike09steelers"
