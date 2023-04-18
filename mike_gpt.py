10937a7f896cda22482ba995f5a9a6ce1d1be1ce
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-18 11:54:24 +0000

    Tuesday, April 18, 2023, 2:54:24 PM Eastern European Summer Time

--- mike_gpt.py
+++ mike_gpt.py
@@ -54,7 +54,7 @@ async def event_message(ctx):
 
             await mfs.send_split_gpt(ctx, output_text)
             print(f"\nGenerated in {elapsed_time:.2f} seconds")
-    await asyncio.sleep(
+    await asyncio.sleep(30)
 
 
 if __name__ == "__main__":
