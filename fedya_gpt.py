a9ba4087b867e2c3110c54e3bc31f3030683d3d1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-18 09:01:09 +0000

    Tuesday, April 18, 2023, 12:01:09 PM Eastern European Summer Time

--- fedya_gpt.py
+++ fedya_gpt.py
@@ -11,11 +11,6 @@ import re
 import mfs
 from vars import *
 
-# for handling logging messages in an appropriate folder
-log_dir = "D:/repos/python-twitchio-chat-bot/logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
 # for handling bot setup
 TMI_TOKEN = "oauth:ks7o8hg39l0qe4rdft8uvm3qgox66m"
 CLIENT_ID = "jdpik06wovybvidhcwd1wplwlgf8cv"
@@ -31,7 +26,6 @@ openai.api_key = "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"
 model_engine = "gpt-3.5-turbo"
 '''
 
-# initialize the bot with the necessary variables
 bot = commands.Bot(
     irc_token=TMI_TOKEN,
     client_id=CLIENT_ID,
@@ -39,7 +33,6 @@ bot = commands.Bot(
     prefix=BOT_PREFIX,
     initial_channels=[CHANNEL]
 )
-
 
 '''OUTDATED
 # declare a function for generating output from ChatGPT
@@ -66,54 +59,40 @@ def generate_response(input_text):
 '''
 
 
-# declare bot event when bot is ready
 @ bot.event
 async def event_ready():
     # print bot and channel name when it activates
     print(f"{BOT_NICK} is online at {CHANNEL}!")
     # log it
-    mfs.write_to_log(f"is online at {CHANNEL}!", " BOT")
+    mfs.write_to_log(f"is online at {CHANNEL}!", BOT_NICK, CHANNEL)
 
 
-# declare bot event on every message
 @ bot.event
 async def event_message(ctx):
-    # handle situations with messages from a bot itself
     if ctx.author.name.lower() == BOT_NICK.lower():
-        # log the message and move on
         print(f"\nBOT: {ctx.content}")
         mfs.write_to_log(ctx.content, BOT_NICK, CHANNEL)
         return
 
-    # for handling ChatGPT requests from chat
     letters = ["@wuyodo"]
-    # check if message contains bot mention
     if mfs.check_for_letters(ctx.content.lower(), letters):
-        # check if the message is not from another bot
         if ctx.author.name.lower() != "pawrop":
-            # print message generation
             print("\nGenerating a message...")
             start_time = time.time()
 
-            # replace the tag with nothingness
             input_text = ctx.content.replace("@wuyodo", "")
-            # avoid any excessive whitespaces
             input_text = " ".join(input_text.split())
 
-            # add user name to the output and tag them
             output_text = "@" + ctx.author.name + ", "
-            # generate the output text using a corresponding functions
             output_text += mfs.generate(input_text, context_fedya)
 
             end_time = time.time()
             elapsed_time = end_time - start_time
 
-            # output the generated message(s) to chat
             await mfs.send_split_gpt(ctx, output_text)
             print(f"\nGenerated in {elapsed_time:.2f} seconds")
     await asyncio.sleep(20)
 
 
 if __name__ == "__main__":
-    # launch bot
     bot.run()
