63c8176d3910c1bb48a48ea7f6ba171ea3149f88
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 09:10:47 +0000

    Saturday, April 22, 2023, 12:10:47 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,67 +1,28 @@
-from twitchio.ext import commands
-from datetime import datetime
-import openai
-import os
-import asyncio
-import time
-import requests
-import re
-import mfs
-from vars import *
+import phind
 
+prompt = 'who won the quatar world cup'
 
-def generate_ua(input_text, context):
-    input_text = input("\nВведіть текст: ")
-    input_text += " Твоя відповідь має бути Українською мовою."
-    url = "https://www.phind.com/api/infer/creative"
-    data = {
-        "question": input_text,
-        "codeContext": context,
-        "options": {
-            "skill": "advanced",
-            "date": "14/04/2023",
-            "language": "uk-UA",
-            "detailed": False,
-            "creative": False,
-            "concise": True,
-            "expert": True
-        }
-    }
-    headers = {
-        "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-        "Accept": "*/*",
-        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Origin": "https://www.phind.com",
-        "Referer": "https://www.phind.com/search?q=Go+vs+Rust+vs+C%2B%2B&c=&source=searchbox&init=true",
-        "sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \";Not A Brand\";v=\"99\"",
-        "sec-ch-ua-mobile": "?0",
-        "Connection": "keep-alive",
-        "sec-ch-ua-platform": "\"macOS\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site"
-    }
-    cookies = {
-        "__cf_bm": "text",
-        "mp_{id}_mixpanel": "text",
-    }
-    response = requests.post(url, json=data, headers=headers, cookies=cookies)
-    # print(response.status_code)
-    if response.status_code != 200:
-        return "Whoops... Something went wrong. Try again later"
+# help needed: not getting newlines from the stream, please submit a PR if you know how to fix this
+# stream completion
+for result in phind.StreamingCompletion.create(
+        model='gpt-4',
+        prompt=prompt,
+        # create search (set actualSearch to False to disable internet)
+        results=phind.Search.create(prompt, actualSearch=True),
+        creative=False,
+        detailed=False,
+        codeContext=''):  # up to 3000 chars of code
 
-    data = response.text
-    text = data.replace("data: ", "")
-    text = text.replace("\n ", " ")
-    text = f'''{text}'''
-    text = text.replace('''\r\n\r\n''', "")
-    text = text.replace('''\r\n\r''', " ")
-    text = text.replace('''`''', "")
-    text = text.replace('''*''', "")
-    return text
+    print(result.completion.choices[0].text, end='', flush=True)
 
+# normal completion
+result = phind.Completion.create(
+    model='gpt-4',
+    prompt=prompt,
+    # create search (set actualSearch to False to disable internet)
+    results=phind.Search.create(prompt, actualSearch=True),
+    creative=False,
+    detailed=False,
+    codeContext='')  # up to 3000 chars of code
 
-while True:
-    print(generate_ua("", context_mike))
+print(result.completion.choices[0].text)
