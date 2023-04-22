4937a2b5d7e2294ed60d455973a6705eed90cd32
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 09:47:21 +0000

    Saturday, April 22, 2023, 12:47:21 PM Eastern European Summer Time

--- mfs.py
+++ mfs.py
@@ -8,6 +8,7 @@ import time
 import requests
 import re
 from vars import *
+import ora
 
 
 def split_long_gpt(input_string):
@@ -172,22 +173,3 @@ def write_to_log(message, author, CHANNEL):
         # output message with author name to log
         log_file.write(f"\n\n{author}: {message}\n")
         log_file.write(f"\n---\n\n")
-
-
-model = ora.CompletionModel.create(
-    system_prompt="Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe.",
-    description='Бот у чаті Twitch стрімера PixelFedya',
-    name='gpt-4')
-
-init = ora.Completion.create(
-    model=model,
-    prompt='привітайся з чатом Феді')
-
-
-def generate_ua(prompt):
-    response = ora.Completion.create(
-        model=model,
-        prompt=prompt,
-        includeHistory=False,  # remember history
-        conversationId=init.id)
-    return response.completion.choices[0].text
