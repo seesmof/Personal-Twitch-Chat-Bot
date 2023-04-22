fb58be7b19df05e0cd92e67ec5df85376580b1c1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 16:25:30 +0000

    Saturday, April 22, 2023, 7:25:30 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import sqlchat
 
-prompt = "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
-prompt += input(": ")
-prompt += " Твоя відповідь має бути Українською мовою."
 for response in sqlchat.StreamCompletion.create(
-        prompt=prompt,
+        prompt='write python code to reverse a string',
         messages=[]):
 
     print(response.completion.choices[0].text, end='')
