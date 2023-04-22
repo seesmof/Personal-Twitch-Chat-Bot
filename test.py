a0a4e4dec37a2e85a973faa352b26f024f1841dd
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 15:43:57 +0000

    Saturday, April 22, 2023, 6:43:57 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,7 +1,32 @@
-import sqlchat
+import you
 
-for response in sqlchat.StreamCompletion.create(
-        prompt='write python code to reverse a string',
-        messages=[]):
+# simple request with links and details
+response = you.Completion.create(
+    prompt="hello world",
+    detailed=True,
+    includelinks=True,)
 
-    print(response.completion.choices[0].text, end='')
+print(response)
+
+# {
+#     "response": "...",
+#     "links": [...],
+#     "extra": {...},
+#         "slots": {...}
+#     }
+# }
+
+# chatbot
+
+chat = []
+
+while True:
+    prompt = input("You: ")
+
+    response = you.Completion.create(
+        prompt=prompt,
+        chat=chat)
+
+    print("Bot:", response["response"])
+
+    chat.append({"question": prompt, "answer": response["response"]})
