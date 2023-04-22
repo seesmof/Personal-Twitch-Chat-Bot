1d6597902c7df44d3cfdb77abdd6d902f6dbf005
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 16:24:10 +0000

    Saturday, April 22, 2023, 7:24:10 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import sqlchat
 
 def generate(prompt):
     prompt += "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
+    prompt += " Твоя відповідь має бути Українською мовою."
     for response in sqlchat.StreamCompletion.create(
             prompt=prompt,
             messages=[]):
@@ -12,9 +13,6 @@ def generate(prompt):
 
 while True:
     print()
-    prompt = "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
-    prompt += input(": ")
-    prompt += " Твоя відповідь має бути Українською мовою."
-    response = ora.Completion.create(model, prompt)
-    print(response.completion.choices[0].text)
+    prompt = input(": ")
+    print(generate(prompt))
     print()
