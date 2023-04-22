d5a23d13899d75571195e5c6949b900d0767962a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 09:30:37 +0000

    Saturday, April 22, 2023, 12:30:37 PM Eastern European Summer Time

--- test/new_test.py
+++ test/new_test.py
@@ -1,22 +1,6 @@
-# import quora (poe) package
-import quora
 
-# create account
-# make shure to set enable_bot_creation to True
-token = quora.Account.create(logging=True, enable_bot_creation=True)
+response = quora.Completion.create(model='gpt-4',
+                                   prompt='hello world',
+                                   token=token)
 
-model = quora.Model.create(
-    token=token,
-    model='gpt-3.5-turbo',  # or claude-instant-v1.0
-    system_prompt='you are ChatGPT a large language model ...'
-)
-
-print(model.name)  # gptx....
-
-# streaming response
-for response in quora.StreamingCompletion.create(
-        custom_model=model.name,
-        prompt='hello world',
-        token=token):
-
-    print(response.completion.choices[0].text)
+print(response.completion.choices[0].text)
