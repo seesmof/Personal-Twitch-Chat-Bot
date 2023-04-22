7cb983406307544318d03a6648497904ec2b13c8
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 16:25:06 +0000

    Saturday, April 22, 2023, 7:25:06 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 import sqlchat
 
+prompt = "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
+prompt += input(": ")
+prompt += " Твоя відповідь має бути Українською мовою."
+for response in sqlchat.StreamCompletion.create(
+        prompt=prompt,
+        messages=[]):
 
-def generate(prompt):
-    prompt += "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
-    prompt += " Твоя відповідь має бути Українською мовою."
-    for response in sqlchat.StreamCompletion.create(
-            prompt=prompt,
-            messages=[]):
-
-        return response.completion.choices[0].text
-
-
-while True:
-    print()
-    prompt = input(": ")
-    print(generate(prompt))
-    print()
+    print(response.completion.choices[0].text, end='')
