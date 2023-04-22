50c5423317d7b36b1ba8d2b5de64c2e5d023e23d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 09:23:50 +0000

    Saturday, April 22, 2023, 12:23:50 PM Eastern European Summer Time

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ import ora
 model = ora.CompletionModel.create(
     system_prompt="Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe.",
     description='Бот у чаті Twitch стрімера PixelFedya',
-    name='gpt-3.5')
+    name='gpt-4')
 
 # init conversation (will give you a conversationId)
 init = ora.Completion.create(
@@ -16,12 +16,13 @@ print(init.completion.choices[0].text)
 
 while True:
     # pass in conversationId to continue conversation
+    or_conversationId = init.id
 
     prompt = input('>>> ')
     response = ora.Completion.create(
         model=model,
         prompt=prompt,
         includeHistory=True,  # remember history
-        conversationId=init.id)
+        conversationId=or_conversationId)
 
     print(response.completion.choices[0].text)
