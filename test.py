65abebe8cf882ac569ef38f954f2c89d4a0157a8
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 16:23:43 +0000

    Saturday, April 22, 2023, 7:23:43 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import sqlchat
 
 
 def generate(prompt):
+    prompt += "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
     for response in sqlchat.StreamCompletion.create(
-            prompt='write python code to reverse a string',
+            prompt=prompt,
             messages=[]):
 
         return (response.completion.choices[0].text, end='')
