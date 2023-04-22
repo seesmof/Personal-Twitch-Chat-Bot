3b56f075f2e7581db36d997ebc7d7d2189bd971b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-22 16:26:30 +0000

    Saturday, April 22, 2023, 7:26:30 PM Eastern European Summer Time

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sqlchat
 
-prompt = (": ")
+prompt = input(": ")
 prompt += "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
 
 for response in sqlchat.StreamCompletion.create(
