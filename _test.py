f2c0ba8edc31713e5a7684058b27b65af9bfbbfa
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 11:02:52 +0000

    Monday, April 24, 2023, 2:02:52 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,29 +1,51 @@
-# inport ora
-import ora
+# import writesonic
+import writesonic
 
-# create model
-model = ora.CompletionModel.create(
-    system_prompt='You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible',
-    description='ChatGPT Openai Language Model',
-    name='gpt-3.5')
+# create account (3-4s)
+account = writesonic.Account.create(logging=True)
 
-print(model.id)
+# with loging:
+# 2023-04-06 21:50:25 INFO __main__ -> register success : '{"id":"51aa0809-3053-44f7-922a...' (2s)
+# 2023-04-06 21:50:25 INFO __main__ -> id : '51aa0809-3053-44f7-922a-2b85d8d07edf'
+# 2023-04-06 21:50:25 INFO __main__ -> token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6Ik...'
+# 2023-04-06 21:50:28 INFO __main__ -> got key : '194158c4-d249-4be0-82c6-5049e869533c' (2s)
 
-# init conversation (will give you a conversationId)
-init = ora.Completion.create(
-    model=model,
-    prompt='hello world')
+# simple completion
+response = writesonic.Completion.create(
+    api_key=account.key,
+    prompt='hello world'
+)
 
-print(init.completion.choices[0].text)
+# Hello! How may I assist you today?
+print(response.completion.choices[0].text)
 
-while True:
-    # pass in conversationId to continue conversation
+# conversation
 
-    prompt = input('>>> ')
-    response = ora.Completion.create(
-        model=model,
-        prompt=prompt,
-        includeHistory=True,
-        conversationId=init.id)
+response = writesonic.Completion.create(
+    api_key=account.key,
+    prompt='what is my name ?',
+    enable_memory=True,
+    history_data=[
+        {
+            'is_sent': True,
+            'message': 'my name is Tekky'
+        },
+        {
+            'is_sent': False,
+            'message': 'hello Tekky'
+        }
+    ]
+)
 
-    print(response.completion.choices[0].text)
+print(response.completion.choices[0].text)  # Your name is Tekky.
+
+# enable internet
+
+response = writesonic.Completion.create(
+    api_key=account.key,
+    prompt='who won the quatar world cup ?',
+    enable_google_results=True
+)
+
+# Argentina won the 2022 FIFA World Cup tournament held in Qatar ...
+print(response.completion.choices[0].text)
