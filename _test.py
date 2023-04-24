61a6e580caa0b94280835e2c1dfa087d17503265
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 11:08:26 +0000

    Monday, April 24, 2023, 2:08:26 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -4,31 +4,17 @@ import phind
 phind.cf_clearance = 'heguhSRBB9d0sjLvGbQECS8b80m2BQ31xEmk9ChshKI-1682268995-0-160'
 phind.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
 
-prompt = input(": ")
+input_prompt = input(": ")
+input_prompt += " Твоя відповідь має бути Українською мовою."
 
 # normal completion
 result = phind.Completion.create(
     model='gpt-4',
-    prompt=prompt,
+    prompt=input_prompt,
     # create search (set actualSearch to False to disable internet)
-    results=phind.Search.create(prompt, actualSearch=False),
+    results=phind.Search.create(input_prompt, actualSearch=False),
     creative=False,
     detailed=False,
     codeContext='')  # up to 3000 chars of code
 
 print(result.completion.choices[0].text)
-
-prompt = input(": ")
-
-# help needed: not getting newlines from the stream, please submit a PR if you know how to fix this
-# stream completion
-for result in phind.StreamingCompletion.create(
-        model='gpt-3.5',
-        prompt=prompt,
-        # create search (set actualSearch to False to disable internet)
-        results=phind.Search.create(prompt, actualSearch=True),
-        creative=False,
-        detailed=False,
-        codeContext=''):  # up to 3000 chars of code
-
-    print(result.completion.choices[0].text, end='', flush=True)
