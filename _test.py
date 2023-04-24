1e1feac00bda8513180a1804aeabff89471432bf
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 13:18:23 +0000

    Monday, April 24, 2023, 4:18:23 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,20 +1,18 @@
-import phind
+import quora
 
-# set cf_clearance cookie
-phind.cf_clearance = 'heguhSRBB9d0sjLvGbQECS8b80m2BQ31xEmk9ChshKI-1682268995-0-160'
-phind.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+token = quora.Account.create(logging=True, enable_bot_creation=True)
 
-input_prompt = input(": ")
-input_prompt += " Твоя відповідь має бути Українською мовою."
+model = quora.Model.create(
+    token=token,
+    model='gpt-3.5-turbo',  # or claude-instant-v1.0
+    system_prompt='you are ChatGPT a large language model ...'
+)
 
-# normal completion
-result = phind.Completion.create(
-    model='gpt-4',
-    prompt=input_prompt,
-    # create search (set actualSearch to False to disable internet)
-    results=phind.Search.create(input_prompt, actualSearch=False),
-    creative=False,
-    detailed=False,
-    codeContext='')  # up to 3000 chars of code
+print(model.name)
 
-print(result.completion.choices[0].text)
+for response in quora.StreamingCompletion.create(
+        custom_model=model.name,
+        prompt='hello world',
+        token=token):
+
+    print(response.completion.choices[0].text)
