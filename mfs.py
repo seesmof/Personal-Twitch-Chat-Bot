7755e6f4a74759ee5de03661eed1b5f997c33297
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 11:22:18 +0000

    Monday, April 24, 2023, 2:22:18 PM Eastern European Summer Time

--- mfs.py
+++ mfs.py
@@ -110,3 +110,23 @@ def phind_ua(input_prompt):
         codeContext='')  # up to 3000 chars of code
 
     return result.completion.choices[0].text
+
+
+def phind_en(input_prompt):
+    input_prompt += " Answer only in English."
+
+    # set cf_clearance cookie
+    phind.cf_clearance = 'heguhSRBB9d0sjLvGbQECS8b80m2BQ31xEmk9ChshKI-1682268995-0-160'
+    phind.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+
+    # normal completion
+    result = phind.Completion.create(
+        model='gpt-4',
+        prompt=input_prompt,
+        # create search (set actualSearch to False to disable internet)
+        results=phind.Search.create(input_prompt, actualSearch=False),
+        creative=False,
+        detailed=False,
+        codeContext='')  # up to 3000 chars of code
+
+    return result.completion.choices[0].text
