903e7a3d0ca56d1f46eb9bf0a7aca36df22ee960
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 13:20:22 +0000

    Monday, April 24, 2023, 4:20:22 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,51 +1,32 @@
-# import writesonic
-import writesonic
+import you
 
-# create account (3-4s)
-account = writesonic.Account.create(logging=True)
+# simple request with links and details
+response = you.Completion.create(
+    prompt="hello world",
+    detailed=True,
+    includelinks=True,)
 
-# with loging:
-# 2023-04-06 21:50:25 INFO __main__ -> register success : '{"id":"51aa0809-3053-44f7-922a...' (2s)
-# 2023-04-06 21:50:25 INFO __main__ -> id : '51aa0809-3053-44f7-922a-2b85d8d07edf'
-# 2023-04-06 21:50:25 INFO __main__ -> token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6Ik...'
-# 2023-04-06 21:50:28 INFO __main__ -> got key : '194158c4-d249-4be0-82c6-5049e869533c' (2s)
+print(response)
 
-# simple completion
-response = writesonic.Completion.create(
-    api_key=account.key,
-    prompt='hello world'
-)
+# {
+#     "response": "...",
+#     "links": [...],
+#     "extra": {...},
+#         "slots": {...}
+#     }
+# }
 
-# Hello! How may I assist you today?
-print(response.completion.choices[0].text)
+# chatbot
 
-# conversation
+chat = []
 
-response = writesonic.Completion.create(
-    api_key=account.key,
-    prompt='what is my name ?',
-    enable_memory=True,
-    history_data=[
-        {
-            'is_sent': True,
-            'message': 'my name is Tekky'
-        },
-        {
-            'is_sent': False,
-            'message': 'hello Tekky'
-        }
-    ]
-)
+while True:
+    prompt = input("You: ")
 
-print(response.completion.choices[0].text)  # Your name is Tekky.
+    response = you.Completion.create(
+        prompt=prompt,
+        chat=chat)
 
-# enable internet
+    print("Bot:", response["response"])
 
-response = writesonic.Completion.create(
-    api_key=account.key,
-    prompt='who won the quatar world cup ?',
-    enable_google_results=True
-)
-
-# Argentina won the 2022 FIFA World Cup tournament held in Qatar ...
-print(response.completion.choices[0].text)
+    chat.append({"question": prompt, "answer": response["response"]})
