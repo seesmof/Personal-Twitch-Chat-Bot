6a3b5d64eedfeb436d44fec5018dc81f6b02b7ff
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-24 13:11:01 +0000

    Monday, April 24, 2023, 4:11:01 PM Eastern European Summer Time

--- _k3ned1_gpt.py
+++ _k3ned1_gpt.py
@@ -11,10 +11,7 @@ from vars import *
 import ora
 
 BOT_PREFIX = "!"
-# CHANNEL = "seesmof"
 CHANNEL = "k3ned1"
-
-# initialize the bot with the necessary variables
 bot = commands.Bot(
     irc_token=GPT_TMI_TOKEN,
     client_id=GPT_CLIENT_ID,
@@ -32,42 +29,27 @@ async def event_ready():
 
 @ bot.event
 async def event_message(ctx):
-    # handle situations with messages from a bot itself
     if ctx.author.name.lower() == GPT_BOT_NICK.lower():
-        # log the message and move on
         print(f"\nBOT: {ctx.content}")
         mfs.write_to_log(ctx.content, GPT_BOT_NICK, CHANNEL)
         return
 
-    # for handling ChatGPT requests from chat
     letters = ["@wuyodo"]
-    # check if message contains bot mention
-    if mfs.check_for_letters(ctx.content.lower(), letters):
-        # check if the message is not from another bot
-        if ctx.author.name.lower() == "k3ned1":
-            # print message generation
-            print("\nGenerating a message...")
-            start_time = time.time()
+    if mfs.check_for_letters(ctx.content.lower(), letters) and ctx.author.name.lower() == "k3ned1":
+        print("\nGenerating a message...")
+        start_time = time.time()
 
-            # replace the tag with nothingness
-            input_text = ctx.content.replace("@wuyodo", "")
-            # avoid any excessive whitespaces
-            input_text = " ".join(input_text.split())
+        input_text = ctx.content.replace("@wuyodo", "")
+        input_text = " ".join(input_text.split())
+        output_text = "@" + ctx.author.name + ", "
+        output_text += mfs.generate_ua(input_text)
 
-            # add user name to the output and tag them
-            output_text = "@" + ctx.author.name + ", "
-            # generate the output text using a corresponding functions
-            output_text += mfs.generate_ua(input_text)
-
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-
-            # output the generated message(s) to chat
-            await mfs.send_split_gpt(ctx, output_text)
-            print(f"\nGenerated in {elapsed_time:.2f} seconds")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        await mfs.send_split_gpt(ctx, output_text)
+        print(f"\nGenerated in {elapsed_time:.2f} seconds")
     await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
-    # launch bot
     bot.run()
