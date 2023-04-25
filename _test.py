346eeaa8e0c49dafbcda075f5503f35fa35bafe4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-25 10:15:25 +0000

    Tuesday, April 25, 2023, 1:15:25 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,15 +1,7 @@
-import ora
+import sqlchat
 
+for response in sqlchat.StreamCompletion.create(
+        prompt='write python code to reverse a string',
+        messages=[]):
 
-# create model
-model = ora.CompletionModel.create(
-    system_prompt='You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible',
-    description='ChatGPT Openai Language Model',
-    name='gpt-3.5')
-
-# init conversation (will give you a conversationId)
-init = ora.Completion.create(
-    model=model,
-    prompt='hello world')
-
-print(init.completion.choices[0].text)
+    print(response.completion.choices[0].text, end='')
