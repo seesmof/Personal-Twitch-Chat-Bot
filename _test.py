e5b512dc609d3381b8a5f581da7535bd2ad397fc
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-26 05:27:42 +0000

    Wednesday, April 26, 2023, 8:27:42 AM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,33 +1,18 @@
-import phind
+import quora
 
-# set cf_clearance cookie (needed again)
-phind.cf_clearance = 'xx.xx-1682166681-0-160'
-# same as the one from browser you got cf_clearance from
-phind.user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
+token = quora.Account.create(logging=True, enable_bot_creation=True)
 
-prompt = 'who won the quatar world cup'
+model = quora.Model.create(
+    token=token,
+    model='gpt-3.5-turbo',  # or claude-instant-v1.0
+    system_prompt='you are ChatGPT a large language model ...'
+)
 
-# help needed: not getting newlines from the stream, please submit a PR if you know how to fix this
-# stream completion
-for result in phind.StreamingCompletion.create(
-        model='gpt-4',
-        prompt=prompt,
-        # create search (set actualSearch to False to disable internet)
-        results=phind.Search.create(prompt, actualSearch=True),
-        creative=False,
-        detailed=False,
-        codeContext=''):  # up to 3000 chars of code
+print(model.name)
 
-    print(result.completion.choices[0].text, end='', flush=True)
+for response in quora.StreamingCompletion.create(
+        custom_model=model.name,
+        prompt='hello world',
+        token=token):
 
-# normal completion
-result = phind.Completion.create(
-    model='gpt-3.5',
-    prompt=prompt,
-    # create search (set actualSearch to False to disable internet)
-    results=phind.Search.create(prompt, actualSearch=True),
-    creative=False,
-    detailed=False,
-    codeContext='')  # up to 3000 chars of code
-
-print(result.completion.choices[0].text)
+    print(response.completion.choices[0].text)
