c31a6b72dd76055f1dae4ff7310c9a4ba868b980
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-26 05:31:43 +0000

    Wednesday, April 26, 2023, 8:31:43 AM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,18 +1,51 @@
-import quora
+# import writesonic
+import writesonic
 
-token = quora.Account.create(logging=True, enable_bot_creation=True)
+# create account (3-4s)
+account = writesonic.Account.create(logging=True)
 
-model = quora.Model.create(
-    token=token,
-    model='gpt-3.5-turbo',  # or claude-instant-v1.0
-    system_prompt='you are ChatGPT a large language model ...'
+# with loging:
+# 2023-04-06 21:50:25 INFO __main__ -> register success : '{"id":"51aa0809-3053-44f7-922a...' (2s)
+# 2023-04-06 21:50:25 INFO __main__ -> id : '51aa0809-3053-44f7-922a-2b85d8d07edf'
+# 2023-04-06 21:50:25 INFO __main__ -> token : 'eyJhbGciOiJIUzI1NiIsInR5cCI6Ik...'
+# 2023-04-06 21:50:28 INFO __main__ -> got key : '194158c4-d249-4be0-82c6-5049e869533c' (2s)
+
+# simple completion
+response = writesonic.Completion.create(
+    api_key=account.key,
+    prompt='hello world'
 )
 
-print(model.name)
+# Hello! How may I assist you today?
+print(response.completion.choices[0].text)
 
-for response in quora.StreamingCompletion.create(
-        custom_model=model.name,
-        prompt='hello world',
-        token=token):
+# conversation
 
-    print(response.completion.choices[0].text)
+response = writesonic.Completion.create(
+    api_key=account.key,
+    prompt='what is my name ?',
+    enable_memory=True,
+    history_data=[
+        {
+            'is_sent': True,
+            'message': 'my name is Tekky'
+        },
+        {
+            'is_sent': False,
+            'message': 'hello Tekky'
+        }
+    ]
+)
+
+print(response.completion.choices[0].text)  # Your name is Tekky.
+
+# enable internet
+
+response = writesonic.Completion.create(
+    api_key=account.key,
+    prompt='who won the quatar world cup ?',
+    enable_google_results=True
+)
+
+# Argentina won the 2022 FIFA World Cup tournament held in Qatar ...
+print(response.completion.choices[0].text)
