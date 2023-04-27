722d527cc9ae0ee2850dbdcb3142b3be136879de
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-27 09:46:53 +0000

    Thursday, April 27, 2023, 12:46:53 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -4,11 +4,10 @@ import vars
 # create an account
 token = forefront.Account.create(logging=True)
 
-inputstr = "Your background: Ти ШІ-бот на ім'я Олекса, створений для стрімера Twitch на ім'я PixelFedya, або просто Федя. Ти ніколи не спілкуєшся і не відповідаєш російською. As an AI assistant, you have three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe, and increase understanding in the universe."
-inputstr += input(": ")
+inputstr = input(": ")
 
 # get a response
 for response in forefront.StreamingCompletion.create(token=token,
-                                                     prompt=inputstr, model='gpt-3.5'):
+                                                     prompt=inputstr, model='gpt-4'):
 
     print(response.completion.choices[0].text, end='')
