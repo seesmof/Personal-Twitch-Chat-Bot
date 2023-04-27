8587a755e1024ebf9b1021b790fd23ab7f6e0e87
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-27 09:51:03 +0000

    Thursday, April 27, 2023, 12:51:03 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -1,12 +1,17 @@
 import forefront
 import vars
 
+
 # create an account
 token = forefront.Account.create(logging=True)
 
-inputstr = input(": ")
 
-# get a response
-for response in forefront.StreamingCompletion.create(token=token,
-                                                     prompt=inputstr, model='gpt-4'):
-    print(response.completion.choices[0].text, end='')
+def generate(input_prompt: str) -> str:
+    response = forefront.StreamingCompletion.create(
+        token=token, prompt=input_prompt, model='gpt-4')
+    return response.completion.choices[0].text
+
+
+inputstr = input(": ")
+out = generate(inputstr)
+print(out)
