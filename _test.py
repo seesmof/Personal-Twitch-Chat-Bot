ad83d3d9bb9d3085cad5db9507c1cd26886002fc
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-26 10:07:35 +0300

    chore: trying to implement chain of thought and reasoning in LLM

--- _test.py
+++ _test.py
@@ -1,18 +1,29 @@
-from gpt4free import deepai
+import gpt4free
+from gpt4free import Provider
+from deep_translator import GoogleTranslator
 
-#single completion
-for chunk in deepai.Completion.create("Write a list of possible vacation destinations:"):
-    print(chunk, end="", flush=True)
-print()
+general_prompt = "Imagine you are a personal AI assistant, which has three heuristic imperatives: reduce suffering in the universe, increase prosperity in the universe and increase understanding in the universe. "
 
-#chat completion
-print("==============")
-messages = [ #taken from the openai docs
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-]
-for chunk in deepai.ChatCompletion.create(messages):
-    print(chunk, end="", flush=True)
-print()
+def make_api_call(input_text):
+    response = gpt4free.Completion.create(
+        Provider.You, prompt=input_text)
+    return response
+
+def break_down_prompt(input_text):
+    input_text += f"Please identify the key words or phrases in the prompt. Output them in the following format: \"key1\", \"key2\", ..."
+    print(f"\n{input_text}\n")
+    result = make_api_call(input_text)
+    return result
+
+
+# def break_down_keyword(keywords):
+    
+
+def main():
+    global general_prompt
+    initial_prompt = input(": ")
+    general_prompt += f"Your goal right now is to solve this problem: \"{initial_prompt}\". The phrase in brackets is your prompt you will be working with, please keep that in mind. "
+    
+    keywords = break_down_prompt(general_prompt)
+
+main()
