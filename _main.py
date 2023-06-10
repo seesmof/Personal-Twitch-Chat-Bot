8192b725e7f05e3c8f734063ef29d1dc11c40b5d
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-10 08:58:34 +0300

    feat: finish up the general bot

--- _main.py
+++ _main.py
@@ -7,7 +7,7 @@ import asyncio
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(token=GPT_TOKEN,
-                         prefix='!', initial_channels=['mike09steelers', 'seesmof', 'PixelFedya'])
+                         prefix='!', initial_channels=['mike09steelers', 'seesmof', 'PixelFedya', 'k3ned1'])
         self.lock = asyncio.Lock()
 
     async def event_ready(self):
@@ -18,12 +18,22 @@ class Bot(commands.Bot):
         async with self.lock:
             letters = ["@piprly", "@wuyodo"]
             if check_for_letters(message.content.lower(), letters) and message.author.name != "piprly":
-                output_text = generate_ai_message(
-                    message.content, message.author.name)
+                output_text = ""
+                if message.channel.name == "k3ned1":
+                    if message.author.name == "k3ned1":
+                        output_text = generate_ai_message(
+                            message.content, message.author.name)
+                    else:
+                        output_text = random.choice(error_ua)
+                else:
+                    output_text = generate_ai_message(
+                        message.content, message.author.name)
                 output_text = split_long_gpt(output_text)
                 for substr in output_text:
                     await message.channel.send(f"{substr}, @{message.author.name}")
                     await asyncio.sleep(20)
+        write_to_log(message.content, message.author.name,
+                     message.channel.name)
 
 
 bot = Bot()
