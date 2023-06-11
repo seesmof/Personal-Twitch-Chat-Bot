0e25db81560b13ae8ea009ed015188441c117dab
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-11 14:24:41 +0000

    🌿: Sunday, June 11, 2023 at 5:24:41 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -3,9 +3,16 @@ from mfs import *
 from gpt4free import usesless
 
 
-input_text = input(": ")
-output_text = generate_ai_message(
-    input_text, "seesmof")
-output_text = split_long_gpt(output_text)
-for substr in output_text:
-    print(f"{substr} @seesmof")
+def useless_ua(input_text):
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDczMzEsImlhdCI6MTY4NjQ5Mjc3MiwiZXhwIjoxNzE4MDUwMzcyfQ.dZdg4CdyS4HuJUD2NSnYQYJCr2o9b5cdIhEo6W5KXGA"
+    message_id = ''
+    input_prompt = GoogleTranslator(
+        source='auto', target='uk').translate(input_text)
+    req = usesless.Completion.create(
+        prompt=input_text, parentMessageId=message_id, token=token)
+    response = req['text']
+    return response
+
+
+inputtext = input(": ")
+print(useless_ua(inputtext))
--- mfs.py
+++ mfs.py
@@ -116,21 +116,23 @@ def gpt4free_en(input_text):
 
 
 def useless_ua(input_text):
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDczMzEsImlhdCI6MTY4NjQ5Mjc3MiwiZXhwIjoxNzE4MDUwMzcyfQ.dZdg4CdyS4HuJUD2NSnYQYJCr2o9b5cdIhEo6W5KXGA"
     message_id = ''
     input_prompt = GoogleTranslator(
         source='auto', target='uk').translate(input_text)
     req = usesless.Completion.create(
-        prompt=input_prompt, parentMessageId=message_id)
+        prompt=input_prompt, parentMessageId=message_id, token=token)
     response = req['text']
     return response
 
 
 def useless_en(input_text):
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDczMzEsImlhdCI6MTY4NjQ5Mjc3MiwiZXhwIjoxNzE4MDUwMzcyfQ.dZdg4CdyS4HuJUD2NSnYQYJCr2o9b5cdIhEo6W5KXGA"
     message_id = ''
     input_prompt = GoogleTranslator(
         source='auto', target='en').translate(input_text)
     req = usesless.Completion.create(
-        prompt=input_prompt, parentMessageId=message_id)
+        prompt=input_prompt, parentMessageId=message_id, token=token)
     response = req['text']
     return response
 
