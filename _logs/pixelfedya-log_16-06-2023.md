50fc335e35a8d07554507cfd178133429e556fe4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-16 08:05:57 +0300

    changed 4 files

---
+++ _logs/pixelfedya-log_16-06-2023.md
@@ -0,0 +1,6 @@
+07:55:08
+
+streamelements: Вся інфа про стріми тут - https://t.me/pixelfedya
+
+---
+
--- _test.py
+++ _test.py
@@ -1,14 +1,7 @@
-from gpt4free.quora import Poe
+import requests
 
-# available models:  ['Sage', 'GPT-4', 'Claude+', 'Claude-instant', 'ChatGPT', 'Dragonfly', 'NeevaAI']
-
-poe = Poe(model='ChatGPT', driver='firefox',
-          cookie_path='cookie.json', driver_path='path_of_driver')
-poe.chat('who won the football world cup most?')
-
-# new bot creation
-poe.create_bot('new_bot_name', prompt='You are new test bot',
-               base_model='gpt-3.5-turbo')
-
-# delete account
-poe.delete_account()
+letters = "f h n s u i s"
+response = requests.get(
+    f"https://wordfinder.yourdictionary.com/unscramble/{letters}")
+words = response.json()["words"]
+print(words)
