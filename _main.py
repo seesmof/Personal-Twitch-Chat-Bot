06b7972fb56ae9f4a0774904f53517c17b92cacd
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-19 15:46:21 +0000

    🌿: Monday, June 19, 2023 at 6:46:21 PM Eastern European Summer Time

--- _main.py
+++ _main.py
@@ -7,7 +7,7 @@ import asyncio
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(token=GPT_TOKEN,
-                         prefix='!', initial_channels=['seesmof', 'PixelFedya', 'k3ned1', 'imabeinthearmyorfootball'])
+                         prefix='!', initial_channels=['seesmof', 'PixelFedya', 'k3ned1', 'unjustfridgesmod'])
         self.lock = asyncio.Lock()
 
     async def event_ready(self):
