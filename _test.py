20a270ba3b169d0075bb7efba8b7d13c0999fc8b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-26 15:56:03 +0300

    changed 267 files

--- _test.py
+++ _test.py
@@ -1,7 +1,27 @@
-import requests
+import g4f
 
-letters = "f h n s u i s"
-response = requests.get(
-    f"https://wordfinder.yourdictionary.com/unscramble/{letters}")
-words = response.json()["words"]
-print(words)
+
+print(g4f.Provider.Ails.params)  # supported args
+
+# Automatic selection of provider
+
+# streamed completion
+response = g4f.ChatCompletion.create(model='gpt-3.5-turbo', messages=[
+                                     {"role": "user", "content": "Hello world"}], stream=True)
+
+for message in response:
+    print(message)
+
+# normal response
+response = g4f.ChatCompletion.create(model=g4f.Model.gpt_4, messages=[
+                                     {"role": "user", "content": "hi"}])  # alterative model setting
+
+print(response)
+
+
+# Set with provider
+response = g4f.ChatCompletion.create(model='gpt-3.5-turbo', provider=g4f.Provider.Forefront, messages=[
+                                     {"role": "user", "content": "Hello world"}], stream=True)
+
+for message in response:
+    print(message)
