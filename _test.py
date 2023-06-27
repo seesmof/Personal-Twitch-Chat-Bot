1f64fb6f145ded54336adbfb8fc2f742ed98dd53
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-27 14:47:07 +0000

    🌿: Tuesday, June 27, 2023 at 5:47:07 PM Eastern European Summer Time

--- _test.py
+++ _test.py
@@ -2,5 +2,13 @@ from mfs import *
 
 while True:
     prompt = input(": ")
-    output = generate_ai_message(prompt)
-    print(output)
+    output = g4f.ChatCompletion.create(
+        model=g4f.Model.gpt_35_turbo,
+        messages=[{
+            "role": "user",
+            "content": prompt
+        }],
+        stream=True
+    )
+    for message in output:
+        print(message, end='')
