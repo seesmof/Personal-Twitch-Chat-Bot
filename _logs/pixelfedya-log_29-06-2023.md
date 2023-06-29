976fd06fbb08226bdaef00d3f67654932f44293a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-29 22:14:40 +0300

    changed 5 files

--- _logs/pixelfedya-log_29-06-2023.md
+++ _logs/pixelfedya-log_29-06-2023.md
@@ -310,3 +310,9 @@ streamelements: Вся інфа про стріми тут - https://t.me/pixelf
 
 ---
 
+22:01:34
+
+streamelements: Вся інфа про стріми тут - https://t.me/pixelfedya
+
+---
+
--- _test.py
+++ _test.py
@@ -2,13 +2,5 @@ from mfs import *
 
 while True:
     prompt = input(": ")
-    output = g4f.ChatCompletion.create(
-        model=g4f.Model.gpt_35_turbo,
-        messages=[{
-            "role": "user",
-            "content": prompt
-        }],
-        stream=True
-    )
-    for message in output:
-        print(message, end='')
+    output = generate_ai_message(prompt)
+    print(output)
